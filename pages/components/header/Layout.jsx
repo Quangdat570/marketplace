24a447f1b647e4b8ba46a8d066264e6cd43175b4
Quang Dat <Quangdat570@gmail.com>
2022-12-19 09:14:18 +0700

    up

--- pages/components/header/Layout.jsx
+++ pages/components/header/Layout.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "../footer/Footer";
-import Headerrrr from "./Test";
-import { Box } from "@mui/material";
+import React from 'react'
+import Header from './Header'
+import Footer from '../footer/Footer'
+import ResponsiveAppBar from './Test'
+
 
 const Layout = (props) => {
   return (
-    <Box>
-      <Header />
-      {props.children}
-      <Footer />
-    </Box>
-  );
-};
+    <div>
+        <ResponsiveAppBar/>
+        {props.children}
+        <Footer/>
+    </div>
+  )
+}
 
-export default Layout;
+export default Layout
